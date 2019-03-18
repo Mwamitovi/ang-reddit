4f8a0d833470282f7bdd28dff55c5bcd1f8c3b50
Martin Matovu <matovu.synergy@gmail.com>
2019-03-18 14:06:50 +0300

    import `Article` , add an  `Article`  instance to store data.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+// import the class "Article"
+import { Article } from './article.model';
+
 
 @Component({
   selector: 'app-article',
@@ -8,22 +11,30 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 export class ArticleComponent implements OnInit {
 
   @HostBinding('attr.class') cssClass = 'row';
-  votes: number;
-  title: string;
-  link: string;
+  // votes: number;
+  // title: string;
+  // link: string;
+  article: Article;
 
   constructor() {
-    this.title = 'MwamiTovi';
-    this.link = 'http://mwamitovi-newsletter.co.ug';
-    this.votes = 10;
+    // this.title = 'MwamiTovi';
+    // this.link = 'http://mwamitovi-newsletter.co.ug';
+    // this.votes = 10;
+    this.article = new Article(
+        'MwamiTovi',
+        'http://mwamitovi-newsletter.co.ug',
+        10
+      );
   }
 
   voteUp(): boolean {
-    this.votes += 1;
+    // this.votes += 1;
+    this.article.votes += 1;
     return false;
   }
   voteDown(): boolean {
-    this.votes -= 1;
+    // this.votes -= 1;
+    this.article.votes -= 1;
     return false;
   }
 
