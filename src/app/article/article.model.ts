5bd96ce9642c4c9b0440a14eef1cc535ae8c7ce0
Martin Matovu <matovu.synergy@gmail.com>
2019-03-18 14:21:11 +0300

    Added methods (voteUp, voteDown,  domain) to `Article` model

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -9,4 +9,21 @@ export class Article {
         this.link = link;
         this.votes = votes || 0;
     }
+
+    voteUp(): void {
+        this.votes += 1;
+    }
+
+    voteDown(): void {
+        this.votes -= 1;
+    }
+
+    domain(): string {
+        try {
+            const domainAndPath: string = this.link.split('//')[1];
+            return domainAndPath.split('/')[0];
+        } catch (err) {
+            return null;
+        }
+    }
 }
