c8f2ffa22fae4dd48d40ed1029aaa5ead38b0cee
Martin Matovu <matovu.synergy@gmail.com>
2019-03-25 16:26:32 +0300

    Added code to list multiple articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'ang-reddit';
+
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('MwamiTovi', 'http://mwamitovi-newsletter.co.ug', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(
       `Adding article title: ${title.value} and link: ${link.value}`
       );
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
+
 }
